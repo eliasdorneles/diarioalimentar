adf3ef8d261cc96048155f0008d92d507caebaa4
José Ricardo <root@josericardo.eti.br>
2012-09-26 18:20:13 -0300

    rails create + rspec:install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Diarioalimentaro::Application.config.secret_token = '1b1222c868b8fe2fdd50faa0c556e3fa3aa3223f84b7e7f1d2c10ec806f0c5be804f2c833250d3f10345ea3a499d7146240dd84f0de2393e39f9c92e387fa755'
